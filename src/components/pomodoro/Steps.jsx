76ff07bc03309bfd719bf9ffdc0ecdb91101f67d
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-05 09:03:43 -0300

    add pomodoro steps

--- src/components/pomodoro/Steps.jsx
+++ src/components/pomodoro/Steps.jsx
@@ -1,7 +1,24 @@
+import PropTypes from 'prop-types'
+
 function Steps() {
   return (
-    <div>Steps</div>
+    <div className="flex flex-centered g-3">
+      <Step />
+      <Step />
+      <Step isActive />
+      <Step />
+    </div>
   )
+}
+
+function Step({ isActive = false }) {
+  return (
+    <button className="pomodoro__step btn" data-is-active={isActive}></button>
+  )
+}
+
+Step.propTypes = {
+  isActive: PropTypes.bool,
 }
 
 export default Steps
