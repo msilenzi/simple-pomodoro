3d1e8f2dc2c98296a04db34f8b85046f14f75f8e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-05 11:02:52 -0300

    add pomodoro controls

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import Pause from './Pause'
+import Play from './Play'
+import Reset from './Reset'
+import Skip from './Skip'
+
+export { Pause, Play, Reset, Skip }
--- src/components/pomodoro/Controls.jsx
+++ src/components/pomodoro/Controls.jsx
@@ -1,7 +1,22 @@
+import PropTypes from 'prop-types'
+import { Play, Reset, Skip } from '@Assets/icons'
+
 function Controls() {
   return (
-    <div>Reset - Play/Pause - Stop</div>
+    <div className="flex flex-centered g-5 mt-4">
+      <Control icon={<Reset />} />
+      <Control icon={<Play />} />
+      <Control icon={<Skip />} />
+    </div>
   )
+}
+
+function Control({ icon }) {
+  return <button className="pomodoro__control btn">{icon}</button>
+}
+
+Control.propTypes = {
+  icon: PropTypes.element.isRequired,
 }
 
 export default Controls
