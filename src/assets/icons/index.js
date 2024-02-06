067c8af297b2cb8f22706086a9c296923033fa8a
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-06 15:45:32 -0300

    add Close icon

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,7 +1,8 @@
+import Close from './Close'
 import Gear from './Gear'
 import Pause from './Pause'
 import Play from './Play'
 import Reset from './Reset'
 import Skip from './Skip'
 
-export { Gear, Pause, Play, Reset, Skip }
+export { Close, Gear, Pause, Play, Reset, Skip }
