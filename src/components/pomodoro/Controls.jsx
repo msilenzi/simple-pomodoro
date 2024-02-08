39350905f28d30239de55b0d9cd6f4b761f95d2e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-08 02:23:52 -0300

    <Controls /> use context

--- src/components/pomodoro/Controls.jsx
+++ src/components/pomodoro/Controls.jsx
@@ -1,11 +1,16 @@
 import PropTypes from 'prop-types'
-import { Play, Reset, Skip } from '@Assets/icons'
+import { Play, Reset, Skip, Pause } from '@Assets/icons'
+import { usePomodoroContext } from '@Hooks'
 
 function Controls() {
+  const {
+    current: { isRunning },
+  } = usePomodoroContext()
+
   return (
     <div className="flex flex-centered g-5 mt-5">
       <Control icon={<Reset />} />
-      <Control icon={<Play />} />
+      <Control icon={isRunning ? <Pause /> : <Play />} />
       <Control icon={<Skip />} />
     </div>
   )
