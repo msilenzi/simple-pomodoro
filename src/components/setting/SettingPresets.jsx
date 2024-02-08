43154023c996b2fcea357f5368a1123b670c2457
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-08 18:11:19 -0300

    change active preset when changing time settings

--- src/components/setting/SettingPresets.jsx
+++ src/components/setting/SettingPresets.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import { pomodoroPresets } from '@Assets/data'
+import { usePomodoroContext } from '@Hooks'
 
 function SettingPresets() {
   return (
@@ -9,9 +10,9 @@ function SettingPresets() {
         {Object.values(pomodoroPresets).map(({ title, times }) => (
           <TimeOption
             title={title}
-            pomodoro={times.pomodoro.toString()}
-            short={times.shortBreak.toString()}
-            long={times.longBreak.toString()}
+            pomodoro={times.pomodoro}
+            shortBreak={times.shortBreak}
+            longBreak={times.longBreak}
             key={title}
           />
         ))}
@@ -20,12 +21,23 @@ function SettingPresets() {
   )
 }
 
-function TimeOption({ title, pomodoro, short, long }) {
+function TimeOption({ title, pomodoro, shortBreak, longBreak }) {
+  const { settings } = usePomodoroContext()
+
+  const isActive =
+    settings.times.pomodoro === pomodoro &&
+    settings.times.shortBreak === shortBreak &&
+    settings.times.longBreak === longBreak
+
   return (
-    <div className="setting__preset p-3" role="button">
+    <div
+      className="setting__preset p-3"
+      data-is-active={isActive}
+      role="button"
+    >
       <p className="fw-700 m-0 mb-2">{title}</p>
       <p className="m-0">
-        {pomodoro} min &bull; {short} min &bull; {long} min
+        {pomodoro} min &bull; {shortBreak} min &bull; {longBreak} min
       </p>
     </div>
   )
@@ -33,9 +45,9 @@ function TimeOption({ title, pomodoro, short, long }) {
 
 TimeOption.propTypes = {
   title: PropTypes.string.isRequired,
-  pomodoro: PropTypes.string.isRequired,
-  short: PropTypes.string.isRequired,
-  long: PropTypes.string.isRequired,
+  pomodoro: PropTypes.number.isRequired,
+  shortBreak: PropTypes.number.isRequired,
+  longBreak: PropTypes.number.isRequired,
 }
 
 export default SettingPresets
