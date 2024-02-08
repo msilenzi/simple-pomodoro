be262c37e287b894ef5c09b8ef6aeeff72f41ed3
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-08 16:15:31 -0300

    add SettingTimes state

--- src/components/setting/SettingTimes.jsx
+++ src/components/setting/SettingTimes.jsx
@@ -1,16 +1,42 @@
 import { InputGroup } from '@Components/ui/form'
+import { usePomodoroContext } from '@Hooks'
+import { useForm } from '@Hooks/useForm'
 
 function SettingTimes() {
+  const { settings } = usePomodoroContext()
+
+  const { formState, handleInputChange } = useForm({
+    pomodoro: settings.times.pomodoro,
+    shortBreak: settings.times.shortBreak,
+    longBreak: settings.times.longBreak,
+  })
+
   return (
     <div className="setting__times mb-4">
-      <InputGroup id="setting-work" type="number" label="Pomodoro" min="1" />
       <InputGroup
-        id="setting-short"
+        id="pomodoro"
+        type="number"
+        label="Pomodoro"
+        min="1"
+        value={formState.pomodoro}
+        onChange={handleInputChange}
+      />
+      <InputGroup
+        id="shortBreak"
         type="number"
         label="Short Break"
         min="1"
+        value={formState.shortBreak}
+        onChange={handleInputChange}
       />
-      <InputGroup id="setting-long" type="number" label="Long Break" min="1" />
+      <InputGroup
+        id="longBreak"
+        type="number"
+        label="Long Break"
+        min="1"
+        value={formState.longBreak}
+        onChange={handleInputChange}
+      />
     </div>
   )
 }
---
+++ src/hooks/useForm.js
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function useForm(initialForm) {
+  const [formState, setFormState] = useState(initialForm)
+
+  function handleInputChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value })
+  }
+
+  return { formState, handleInputChange }
+}
+
+export { useForm }
